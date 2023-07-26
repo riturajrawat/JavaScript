516006bd57b550a0372e88e0ffecf76b2dc57529
Rituraj Rawat <rituraj3001@gmail.com>
2023-07-27 03:05:22 +0530

    Add files via upload

---
+++ 08_Arithmetic_operators/arithmetic_operators.js
@@ -0,0 +1,10 @@
+let numOne = 4
+let numTwo = 3
+let sum = numOne + numTwo // gives the sum of two numbers
+let diff = numOne - numTwo // gives the difference
+let mult = numOne * numTwo // gives the product of a multiplication
+let div = numOne / numTwo // gives the quotient of a division
+let remainder = numOne % numTwo // gives the remainder of a division
+let powerOf = numOne ** numTwo // 4 to the power of 3
+
+console.log(sum, diff, mult, div, remainder, powerOf) // 7,1,12,1.33,1, 64
