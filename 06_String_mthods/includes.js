0b60a4115708f67023e9c9187233bd0c6891ddbf
Rituraj Rawat <rituraj3001@gmail.com>
2023-08-18 23:41:50 +0530

    Update includes.js

--- 06_String_mthods/includes.js
+++ 06_String_mthods/includes.js
@@ -1,6 +1,6 @@
 // includes(): It takes a substring argument and it check if substring argument exists in the string. includes() returns a boolean. It checks if a substring exist in a string and it returns true if it exists and false if it doesn't exist.
 let string = '30 Days Of JavaScript'
-console.log(string.includes('Days'))     // true
+console.log(string.includes('Days'))     // tru
 console.log(string.includes('days'))     // false
 console.log(string.includes('Script'))     // true
 console.log(string.includes('script'))     // false
